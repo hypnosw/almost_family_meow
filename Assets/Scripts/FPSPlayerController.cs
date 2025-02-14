d8ed6fd537d6fe9a085ca9a2f4cabd120dc4b307
hypnosw <hypnoswu@gmail.com>
2025-02-13 22:16:26 -0800

    player control script set up, basic prefabs set up

---
+++ Assets/Scripts/FPSPlayerController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class FPSPlayerController : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float jumpHeight = 0.4f;
+
+    public float gravity = 9.81f;
+    private Vector3 input;
+    Vector3 moveDirection;
+    CharacterController controller;
+    public float airControl = 2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        input = transform.right * moveHorizontal + transform.forward * moveVertical;
+        input.Normalize();
+
+
+        if (controller.isGrounded)
+        {
+            moveDirection = input;
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = Mathf.Sqrt(2 * jumpHeight * gravity);
+
+            }
+            else
+            {
+                moveDirection.y = 0.0f;
+            }
+        }
+        else
+        {
+            input.y = moveDirection.y;
+            moveDirection = Vector3.Lerp(moveDirection, input, airControl * Time.deltaTime);
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * speed * Time.deltaTime);
+    }
+}
